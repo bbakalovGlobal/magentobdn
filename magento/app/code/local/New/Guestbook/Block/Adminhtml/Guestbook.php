3419b73821a4e9cfdb795c959e73805e308403e4
bdn <bakalov.bogdan@gmail.com>
2015-01-21 17:15:08 +0200

    11

--- magento/app/code/local/New/Guestbook/Block/Adminhtml/Guestbook.php
+++ magento/app/code/local/New/Guestbook/Block/Adminhtml/Guestbook.php
@@ -1,9 +1,21 @@
 <?php
 class New_Guestbook_Block_Adminhtml_Guestbook extends Mage_Adminhtml_Block_Abstract
 {
-    public function _toHtml()
+    protected function _construct()
     {
-        return '<h1>News Module: Admin section</h1>';
+        parent::_construct();
+
+        $helper = Mage::helper('guestbook');
+        $this->_blockGroup = 'guestbook';
+        $this->_controller = 'adminhtml_guestbook';
+
+        $this->_headerText = $helper->__('News Management');
+        $this->_addButtonLabel = $helper->__('Add News');
+
     }
+//    public function _toHtml()
+//    {
+//        return '<h1>News Module: Admin section</h1>';
+//    }
 
 }
---
+++ magento/app/code/local/New/Guestbook/Block/Adminhtml/Guestbook/Grid.php
@@ -0,0 +1,39 @@
+<?php
+
+class New_Guestbook_Block_Adminhtml_Guestbook_Grid extends Mage_Adminhtml_Block_Widget_Grid
+{
+
+    protected function _prepareCollection()
+    {
+        $collection = Mage::getModel('guestbook/guestbookposts')->getCollection();
+
+        $this->setCollection($collection);
+        return parent::_prepareCollection();
+    }
+
+    protected function _prepareColumns()
+    {
+
+        $helper = Mage::helper('guestbook');
+
+        $this->addColumn('post_id', array(
+            'header' => $helper->__('Post ID'),
+            'index' => 'post_id'
+        ));
+
+        $this->addColumn('name', array(
+            'header' => $helper->__('Name'),
+            'index' => 'name',
+            'type' => 'text',
+        ));
+
+        $this->addColumn('timestamp', array(
+            'header' => $helper->__('Created'),
+            'index' => 'timestamp',
+            'type' => 'date',
+        ));
+
+        return parent::_prepareColumns();
+    }
+
+}
