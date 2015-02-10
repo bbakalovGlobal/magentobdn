b14978567dfcdd637ec5ec71227cb14f2f6b6cf0
vitalii <vitalsash83@gmail.com>
2015-02-10 22:36:44 +0200

    Fixed 3013

--- magento/app/code/local/Tim/PriceRules/Block/Cart.php
+++ magento/app/code/local/Tim/PriceRules/Block/Cart.php
@@ -17,10 +17,16 @@ class Tim_PriceRules_Block_Cart extends Mage_Checkout_Block_Cart
      */
     public function getItemHtml(Mage_Sales_Model_Quote_Item $item)
     {
-        if ($item->getPrice() < $item->getProduct()->getPrice()) {
-            $item->setCustomPrice($item->getPrice());
+        if (($item->getDiscountAmount() > 0) || ($item->getPrice() < $item->getProduct()->getPrice())) {
+            if ($item->getDiscountAmount() > 0) {
+                $discountPrice = $item->getPrice() - ($item->getDiscountAmount() / $item->getQty());
+                $item->setCustomPrice($discountPrice);
+                $item->setRowTotal($discountPrice * $item->getQty());
+            } else {
+                $item->setCustomPrice($item->getPrice());
+                $item->setRowTotal($item->getPrice() * $item->getQty());
+            }
             $item->setOriginalRowTotal($item->getProduct()->getPrice() * $item->getQty());
-            $item->setRowTotal($item->getPrice() * $item->getQty());
         }
         $renderer = $this->getItemRenderer($item->getProductType())->setItem($item);
         return $renderer->toHtml();
--- magento/app/code/local/Tim/PriceRules/Block/Onepage/Review/Info.php
+++ magento/app/code/local/Tim/PriceRules/Block/Onepage/Review/Info.php
@@ -17,10 +17,16 @@ class Tim_PriceRules_Block_Onepage_Review_Info extends Mage_Checkout_Block_Onepa
      */
     public function getItemHtml(Varien_Object $item)
     {
-        if ($item->getPrice() < $item->getProduct()->getPrice()) {
-            $item->setCustomPrice($item->getPrice());
+        if (($item->getDiscountAmount() > 0) || ($item->getPrice() < $item->getProduct()->getPrice())) {
+            if ($item->getDiscountAmount() > 0) {
+                $discountPrice = $item->getPrice() - ($item->getDiscountAmount() / $item->getQty());
+                $item->setCustomPrice($discountPrice);
+                $item->setRowTotal($discountPrice * $item->getQty());
+            } else {
+                $item->setCustomPrice($item->getPrice());
+                $item->setRowTotal($item->getPrice() * $item->getQty());
+            }
             $item->setOriginalRowTotal($item->getProduct()->getPrice() * $item->getQty());
-            $item->setRowTotal($item->getPrice() * $item->getQty());
         }
         $type = $this->_getItemType($item);
 
